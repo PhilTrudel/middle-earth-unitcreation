cb28e00fa425890ab7ba3d2ec931a3ed06abe5a6
PhilTrudel <61752595+PhilTrudel@users.noreply.github.com>
2021-09-18 16:37:51 -0400

    Changes in preparation for JS

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import '/main.scss'
+
+let addMagic = document.querySelector('.magic');
+let addItem = document.querySelector('.item');
+
+
+addMagic.addEventListener("click", function() {
+    
+});
+
+addItem.addEventListener("click", function() {
+    
+});
