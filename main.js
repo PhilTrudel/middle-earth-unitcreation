1b4ad7de7265f7e47fabd9f675a8393025017f0f
Philippe Trudel <61752595+PhilTrudel@users.noreply.github.com>
2021-09-20 16:22:13 -0400

    Function add-remove Hero table row

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let addMagic = document.querySelector('.magic');
 let addItem = document.querySelector('.item');
 let removeMagic = document.querySelector('.lessMagic');
 let removeItem = document.querySelector('.lessItem');
+let addHero = document.querySelector('.addHero');
+let removeHero = document.querySelector('.removeHero');
 
 
 addMagic.addEventListener("click", function() {
@@ -20,10 +22,28 @@ addItem.addEventListener("click", function() {
     const newRow = document.createElement('tr')
     newRow.className = "text-center"
     newRow.innerHTML = `
-    <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
-    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <th scope="row"><input type="text" class="form-control" placeholder="Item"></th>
     <td><input type="text" class="form-control" placeholder="..."></td>`
     document.getElementById('tableItem').appendChild(newRow);
+});
+
+addHero.addEventListener("click", function() {
+    const newRow = document.createElement('tr')
+    newRow.className = "text-center"
+    newRow.innerHTML = `
+    <th scope="row"><input type="text" class="form-control-xs" placeholder="Hero"></th>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0/9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>`
+    document.getElementById('tableHero').appendChild(newRow);
 });
 
 removeMagic.addEventListener("click", function() {
@@ -34,4 +54,9 @@ removeMagic.addEventListener("click", function() {
 removeItem.addEventListener("click", function() {
     const tableItem = document.getElementById('tableItem')
     tableItem.removeChild(tableItem.lastElementChild)
+});
+
+removeHero.addEventListener("click", function() {
+    const tableHero = document.getElementById('tableHero')
+    tableHero.removeChild(tableHero.lastElementChild)
 });
