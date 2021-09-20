52e8231a499087bbaa9509ab4c85b4e2a177b878
Raphaël Précourt-Senécal <raphaelpsenecal@gmail.com>
2021-09-20 15:19:41 -0400

    add remove row button for magical ower and items

--- main.js
+++ main.js
@@ -2,44 +2,36 @@ import '/main.scss'
 
 let addMagic = document.querySelector('.magic');
 let addItem = document.querySelector('.item');
-let tableMagic = document.querySelector('.tableMagic');
-let tableItem = document.querySelector('.tableItem');
 let removeMagic = document.querySelector('.lessMagic');
 let removeItem = document.querySelector('.lessItem');
 
 
 addMagic.addEventListener("click", function() {
-    tableMagic.innerHTML +=
-    `<tr class="text-center">
+    const newRow = document.createElement('tr')
+    newRow.className = "text-center"
+    newRow.innerHTML = `
     <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
     <td><input type="text" class="form-control" placeholder="0-9"></td>
-    <td><input type="text" class="form-control" placeholder="..."></td>
-    </tr>`;
+    <td><input type="text" class="form-control" placeholder="..."></td>`
+    document.getElementById('tableMagic').appendChild(newRow);
 });
 
 addItem.addEventListener("click", function() {
-    tableItem.innerHTML +=
-    `<tr class="text-center">
-    <th scope="row"><input type="text" class="form-control" placeholder="Item"></th>
-    <td><input type="text" class="form-control" placeholder="..."></td>
-    </tr>`
-    
+    const newRow = document.createElement('tr')
+    newRow.className = "text-center"
+    newRow.innerHTML = `
+    <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="..."></td>`
+    document.getElementById('tableItem').appendChild(newRow);
 });
 
 removeMagic.addEventListener("click", function() {
-    tableMagic.innerHTML -=
-    `<tr class="text-center">
-    <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
-    <td><input type="text" class="form-control" placeholder="0-9"></td>
-    <td><input type="text" class="form-control" placeholder="..."></td>
-    </tr>`;
+    const tableMagic = document.getElementById('tableMagic')
+    tableMagic.removeChild(tableMagic.lastElementChild)
 });
 
 removeItem.addEventListener("click", function() {
-    tableItem.innerHTML -=
-    `<tr class="text-center">
-    <th scope="row"><input type="text" class="form-control" placeholder="Item"></th>
-    <td><input type="text" class="form-control" placeholder="..."></td>
-    </tr>`
-    
+    const tableItem = document.getElementById('tableItem')
+    tableItem.removeChild(tableItem.lastElementChild)
 });
