52c8e1e6f781e6b1414227d083023fa58f5da44c
Philippe Trudel <61752595+PhilTrudel@users.noreply.github.com>
2021-09-20 10:04:23 -0400

    function to add table row

--- main.js
+++ main.js
@@ -2,12 +2,44 @@ import '/main.scss'
 
 let addMagic = document.querySelector('.magic');
 let addItem = document.querySelector('.item');
+let tableMagic = document.querySelector('.tableMagic');
+let tableItem = document.querySelector('.tableItem');
+let removeMagic = document.querySelector('.lessMagic');
+let removeItem = document.querySelector('.lessItem');
 
 
 addMagic.addEventListener("click", function() {
-    
+    tableMagic.innerHTML +=
+    `<tr class="text-center">
+    <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="..."></td>
+    </tr>`;
 });
 
 addItem.addEventListener("click", function() {
+    tableItem.innerHTML +=
+    `<tr class="text-center">
+    <th scope="row"><input type="text" class="form-control" placeholder="Item"></th>
+    <td><input type="text" class="form-control" placeholder="..."></td>
+    </tr>`
+    
+});
+
+removeMagic.addEventListener("click", function() {
+    tableMagic.innerHTML -=
+    `<tr class="text-center">
+    <th scope="row"><input type="text" class="form-control" placeholder="Power"></th>
+    <td><input type="text" class="form-control" placeholder="0-9"></td>
+    <td><input type="text" class="form-control" placeholder="..."></td>
+    </tr>`;
+});
+
+removeItem.addEventListener("click", function() {
+    tableItem.innerHTML -=
+    `<tr class="text-center">
+    <th scope="row"><input type="text" class="form-control" placeholder="Item"></th>
+    <td><input type="text" class="form-control" placeholder="..."></td>
+    </tr>`
     
 });
